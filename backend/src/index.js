0d37f82247b086c4947a7c7edbde2dc7a6d78ccf
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-05-02 00:26:48 -0300

    New DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const path = require('path')
-const cors = require('cors')
+const path = require("path");
+const cors = require("cors");
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 // Altere o código abaixo para conectar no banco de dado
 // do mongodb na nuvem
-mongoose.connect(`mongodb://localhost:27017/bdsi`, { useNewUrlParser: true })
+mongoose.connect(
+    `mongodb+srv://admin:admin@bookcollector-htl2m.mongodb.net/bookcollector?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use((req, res, next) => {
-  req.io = io;
-  next();
-})
+    req.io = io;
+    next();
+});
 
-app.use(cors())
+app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resizes')))
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "..", "uploads", "resizes"))
+);
 
-app.use(require('./routes'))
+app.use(require("./routes"));
 
 server.listen(3333, () => {
-  console.log('Servidor rodando')
-})
-
+    console.log("Servidor rodando");
+});
